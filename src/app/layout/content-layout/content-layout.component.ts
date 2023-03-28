7dd8dce570a37b64f6ad811aba67ba20ad00731d
Leahu Vlad Iulius <leahu.vlad.iulius@gmail.com>
2023-03-28 17:43:05 +0300

    Add colors for temperature and humidity

--- src/app/layout/content-layout/content-layout.component.ts
+++ src/app/layout/content-layout/content-layout.component.ts
@@ -3,13 +3,10 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-content-layout',
   templateUrl: './content-layout.component.html',
-  styleUrls: ['./content-layout.component.scss']
+  styleUrls: ['./content-layout.component.scss'],
 })
 export class ContentLayoutComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
--- src/app/room-climate/room-climate.component.ts
+++ src/app/room-climate/room-climate.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 import { first, interval } from 'rxjs';
 import { RoomClimateData } from '../data/schema/roomClimateData';
 import { EnvironmentService } from '../data/service/environment.service';
@@ -10,16 +10,38 @@ import { EnvironmentService } from '../data/service/environment.service';
 })
 export class RoomClimateComponent implements OnInit {
   roomClimateData: RoomClimateData | undefined;
+  temperatureColor: String | undefined;
+  humidityColor: String | undefined;
+  alert: boolean = false;
 
   constructor(private environmentService: EnvironmentService) {}
 
   ngOnInit(): void {
+    this.roomClimateData = {
+      temperature: 31,
+      humidity: 50,
+      time: new Date(),
+    };
+
+    this.temperatureColor =
+      this.roomClimateData.temperature > 30 ? 'red' : 'green';
+    this.humidityColor = this.roomClimateData.humidity > 60 ? 'red' : 'green';
+    this.alert =
+      this.roomClimateData.temperature > 30 ||
+      this.roomClimateData.humidity > 60;
+
     interval(5000).subscribe(() =>
       this.environmentService
         .getRoomClimateData()
         .pipe(first())
         .subscribe((data) => {
           this.roomClimateData = data;
+          this.temperatureColor = data.temperature > 30 ? 'red' : 'green';
+          this.humidityColor =
+            this.roomClimateData.humidity > 60 ? 'red' : 'green';
+          this.alert =
+            this.roomClimateData.temperature > 30 ||
+            this.roomClimateData.humidity > 60;
         })
     );
   }

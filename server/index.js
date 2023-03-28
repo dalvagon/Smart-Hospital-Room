070c5a367ead09c4d89c486c39936db97d0e213e
raresbancescu <75622881+raresbancescu@users.noreply.github.com>
2023-03-28 19:30:06 +0300

    more endpoints

--- server/index.js
+++ server/index.js
@@ -7,13 +7,7 @@ const { execute, getAllTemperatures, getAllHumidity } = require("./functions");
 app.use(cors());
 
 app.get("/api/data", async (req, res) => {
-  let response = await execute().then((response)=>{
-    console.log(response);
-    res.send(response);
-  }).catch((response)=>{
-    console.log("error")
-    res.send("eroare")
-  })
+  let response = await execute()
   console.log(response);
   res.send(response);
 });
